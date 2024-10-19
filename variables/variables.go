443a5c235dc3d48bec85cb866af0fc2a2fa90d0d
pavan . <150579399+FJZzv@users.noreply.github.com>
2024-10-19 20:12:46 +0530

    Create variables.go

---
+++ variables/variables.go
@@ -0,0 +1,15 @@
+/*
+this is the directory of go variables advent of code ..
+
+*/
+package variables
+import "fmt"
+func main(){
+  var year int = 2024
+  var age int = 20 
+  var name string = "pawan"
+  var x= 18
+  // type inference
+  var ageofpawan=19
+  fmt.Println(ageofpawan)
+  }
